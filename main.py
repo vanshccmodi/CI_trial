fc1627e29aa515c367c130d4e47d48e564657ce2
VANSH MODI N. <modivanshnikunjkumar@gmail.com>
2026-02-17 10:43:09 +0530

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print(f"1 + 2 = {add(1, 2)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
--- test_main.py
+++ test_main.py
@@ -1,6 +1,11 @@
-from main import add
+from main import add, subtract
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 1) == 0
+    assert subtract(0, 5) == -5
